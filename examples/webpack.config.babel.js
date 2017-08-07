5f5e68232a76ac83b688ba122a60a64bfd587ae7
Ben Hodgson <ben@someaddress.net>
2017-08-07 22:09:42 +0100

    Uglify example bundle

--- examples/webpack.config.babel.js
+++ examples/webpack.config.babel.js
@@ -1,38 +1,45 @@
-import path from 'path';
-import webpack from 'webpack';
-import CleanWebpackPlugin from 'clean-webpack-plugin';
+import path from 'path'
+import webpack from 'webpack'
+import CleanWebpackPlugin from 'clean-webpack-plugin'
+import UglifyJSPlugin from 'uglifyjs-webpack-plugin'
 
 export default () => ({
-    entry: {
-        index: path.join(__dirname, './index.js'),
-    },
+  entry: {
+    index: path.join(__dirname, './index.js'),
+  },
 
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
-    },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
 
-    module: {
-        rules: [
-            {
-                test: /.jsx?$/,
-                exclude: /node_modules/,
-                use: [
-                    { loader: 'babel-loader' }
-                ]
-            },
-            {
-              test: /\.(css)$/,
-              loaders: [
-                'style-loader',
-                'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]'
-              ]
-            }
+  module: {
+    rules: [
+      {
+        test: /.jsx?$/,
+        exclude: /node_modules/,
+        use: [
+          { loader: 'babel-loader' }
         ]
-    },
-
-    plugins: [
-        // Clean dist folder
-        new CleanWebpackPlugin(['./dist/build.js']),
+      },
+      {
+        test: /\.(css)$/,
+        loaders: [
+          'style-loader',
+          'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]'
+        ]
+      }
     ]
-});
+  },
+
+  plugins: [
+    new UglifyJSPlugin({
+      sourceMap: false,
+      compressor: {
+        screw_ie8: true,
+        warnings: false,
+      },
+    }),
+    new CleanWebpackPlugin(['./dist/build.js']),
+  ]
+})
